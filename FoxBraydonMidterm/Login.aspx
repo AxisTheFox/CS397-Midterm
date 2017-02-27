9d8a59c9916a92a3bce929f72b116883e077b229
Braydon <foxden00@gmail.com>
2017-02-26 20:36:31 -0500

    Add project files.

---
+++ FoxBraydonMidterm/Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FoxBraydonMidterm.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Log In</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <h1>Log In</h1>
+
+            <p>Username
+                &nbsp&nbsp&nbsp
+                <asp:TextBox ID="usernameTextBox" runat="server"></asp:TextBox></p>
+
+            <p>Password
+                &nbsp&nbsp&nbsp
+                <asp:TextBox ID="passwordTextBox" runat="server" TextMode="Password"></asp:TextBox></p>
+
+            <p>
+                <asp:Button ID="loginButton" runat="server" Text="Log In" OnClick="loginButton_Click" /></p>
+
+            <p>
+                <asp:Label ID="loginErrorLabel" runat="server" Text="" ForeColor="Red"></asp:Label></p>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ FoxBraydonMidterm/Login.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FoxBraydonMidterm
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        int userId = 0;
+        string hashedPasswordFromDatabase = "";
+        string userType = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            executeLoginDatabaseQuery();
+            string hashedPasswordFromUser = getHashedPassword();
+            if (hashedPasswordFromUser.Equals(hashedPasswordFromDatabase))
+            {
+                Session["userId"] = userId;
+                Session["userType"] = userType;
+                redirectByUserType();
+            }
+            else
+            {
+                loginErrorLabel.Text = "You've entered an invalid username or password.";
+            }
+        }
+
+        private void executeLoginDatabaseQuery()
+        {
+            string databaseQuery = "select [UserID], [Password], [Type] from Users where [Login]=@l";
+            OleDbConnection databaseConnection = new OleDbConnection(ConfigurationManager.ConnectionStrings["QA"].ConnectionString);
+            OleDbCommand databaseCommand = new OleDbCommand(databaseQuery, databaseConnection);
+            databaseCommand.Parameters.AddWithValue("@l", usernameTextBox.Text);
+            databaseConnection.Open();
+            OleDbDataReader dataReader = databaseCommand.ExecuteReader();
+            readData(dataReader);
+            dataReader.Close();
+            databaseConnection.Close();
+        }
+
+        private void readData(OleDbDataReader dataReader)
+        {
+            while (dataReader.Read())
+            {
+                userId = Int32.Parse(dataReader["UserID"].ToString());
+                hashedPasswordFromDatabase = dataReader["Password"].ToString();
+                userType = dataReader["Type"].ToString();
+            }
+        }
+
+        private string getHashedPassword()
+        {
+            SHA1CryptoServiceProvider sha1Provider = new SHA1CryptoServiceProvider();
+            string passwordFromUser = passwordTextBox.Text;
+            sha1Provider.ComputeHash(ASCIIEncoding.ASCII.GetBytes(passwordFromUser));
+            byte[] result = sha1Provider.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+            return strBuilder.ToString();
+        }
+
+        private void redirectByUserType()
+        {
+            switch (userType)
+            {
+                case "Tester":
+                    Response.Redirect("Tester.aspx");
+                    break;
+                case "Manager":
+                    Response.Redirect("Manager.aspx");
+                    break;
+                case "Developer":
+                    Response.Redirect("Developer.aspx");
+                    break;
+                case "Adminstrator":
+                    Response.Redirect("Administrator.aspx");
+                    break;
+            }
+        }
+    }
+}
