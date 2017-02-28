d084d425ae40845d0a78bfc9b18ce8bf504fa3eb
Braydon <foxden00@gmail.com>
2017-02-28 10:46:27 -0500

    Implemented and refactored functionality for Tester page.

--- FoxBraydonMidterm/Tester.aspx
+++ FoxBraydonMidterm/Tester.aspx
@@ -4,12 +4,35 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Tester Page</title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        
+        <h1>Tester Page</h1>
+
+        <h2>Enter a new bug:</h2>
+
+        <p>Priority<br />
+            <asp:DropDownList ID="priorityDropDownList" runat="server" AutoPostBack="True">
+                <asp:ListItem>Low</asp:ListItem>
+                <asp:ListItem>Medium</asp:ListItem>
+                <asp:ListItem>High</asp:ListItem>
+                </asp:DropDownList></p>
+
+        <p>Subject<br />
+            <asp:TextBox ID="subjectTextBox" runat="server" Width="40%"></asp:TextBox></p>
+
+        <p>Description<br />
+            <asp:TextBox ID="descriptionTextBox" runat="server" Width="40%"></asp:TextBox></p>
+
+        <p>
+            <asp:Button ID="submitButton" runat="server" Text="Submit Bug" OnClick="submitButton_Click" /></p>
+
+        <h3>
+            <asp:Label ID="submissionResultLabel" runat="server" Text=""></asp:Label></h3>
+
     </div>
     </form>
 </body>
--- FoxBraydonMidterm/Tester.aspx.cs
+++ FoxBraydonMidterm/Tester.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,98 @@ namespace FoxBraydonMidterm
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (userNotAuthorized())
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
 
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            if(!emptyFieldsExist())
+            {
+                attemptBugSubmission();
+            }
+            else
+            {
+                showEmptyFieldsErrorMessage();
+            }
+        }
+
+        private bool emptyFieldsExist()
+        {
+            return subjectTextBox.Text.Equals("") || descriptionTextBox.Text.Equals("");
+        }
+
+        private void attemptBugSubmission()
+        {
+            try
+            {
+                sendBugInfoToDatabase();
+                showSuccessMessage();
+            }
+            catch
+            {
+                showFailureMessage();
+            }
+        }
+
+        private void showEmptyFieldsErrorMessage()
+        {
+            submissionResultLabel.ForeColor = System.Drawing.Color.Red;
+            submissionResultLabel.Text = "All fields must be filled out to submit a bug.";
+        }
+
+        private void sendBugInfoToDatabase()
+        {
+            string databaseQuery = "Insert into Bugs ([EnteredBy], [Subject], [Priority], [Description], [Status]) values (@ent, @subj, @pri, @desc, @stat)";
+            OleDbConnection databaseConnection = new OleDbConnection(ConfigurationManager.ConnectionStrings["QA"].ConnectionString);
+            OleDbCommand databaseCommand = new OleDbCommand(databaseQuery, databaseConnection);
+            addQueryParameters(databaseCommand);
+            runInsertQuery(databaseConnection, databaseCommand);
+        }
+
+        private void addQueryParameters(OleDbCommand databaseCommand)
+        {
+            databaseCommand.Parameters.AddWithValue("@ent", Session["userId"].ToString());
+            databaseCommand.Parameters.AddWithValue("@subj", subjectTextBox.Text);
+            databaseCommand.Parameters.AddWithValue("@pri", priorityDropDownList.SelectedValue.ToString());
+            databaseCommand.Parameters.AddWithValue("@desc", descriptionTextBox.Text);
+            databaseCommand.Parameters.AddWithValue("@stat", "Open");
+        }
+
+        private static void runInsertQuery(OleDbConnection databaseConnection, OleDbCommand databaseCommand)
+        {
+            databaseConnection.Open();
+            databaseCommand.ExecuteNonQuery();
+            databaseConnection.Close();
+        }
+
+        private void showSuccessMessage()
+        {
+            submissionResultLabel.ForeColor = System.Drawing.Color.ForestGreen;
+            submissionResultLabel.Text = "Bug successfully submitted.";
+        }
+
+        private void showFailureMessage()
+        {
+            submissionResultLabel.ForeColor = System.Drawing.Color.Red;
+            submissionResultLabel.Text = "Failed to submit bug to database.";
+        }
+
+        private bool userNotAuthorized()
+        {
+            return userNotLoggedIn() || userNotATester();
+        }
+
+        private bool userNotLoggedIn()
+        {
+            return Session["userType"] == null;
+        }
+
+        private bool userNotATester()
+        {
+            return Session["userType"].ToString() != "Tester";
         }
     }
 }
